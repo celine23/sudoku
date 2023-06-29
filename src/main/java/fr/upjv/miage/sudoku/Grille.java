acf718d55301c7576bd0ba23087c11176337ba30
alice61015 <31961997+celine23@users.noreply.github.com>
2023-06-29 21:43:21 +0200

    Update Grille.java

--- src/main/java/fr/upjv/miage/sudoku/Grille.java
+++ src/main/java/fr/upjv/miage/sudoku/Grille.java
@@ -21,7 +21,8 @@ import fr.upjv.miage.exception.ValeurInitialeModificationException;
 public interface Grille {
 
     /**
-     * Renvoie les ElementDeGrille pouvant exister dans le grille.
+     * @return Renvoie les ElementDeGrille
+     * pouvant exister dans le grille.
      */
     Set<ElementDeGrille> getElements();
 
@@ -31,27 +32,31 @@ public interface Grille {
     int getDimension();
 
     /**
-     * Affecte une valeur dans une case de la grille, ou null pour 'vider' la case
+     * Affecte une valeur dans une case de la grille,
+     * ou null pour 'vider' la case.
      *
-     * @param x     position x dans la grille
-     * @param y     position y dans la grille
-     * @param value élément de grille à mettre dans la case, null pour vider la case
-     * @throws ValeurImpossibleException           si l'élément de grille n'est pas autorisé à cette position dans la grille
-     *                                             aux vues des autres valeurs de la grille
-     * @throws ElementInterditException            si l'élément de grille n'est pas autorisé dans cette grille pouvant être mis dans la grille
-     * @throws HorsBornesException                 si x ou y sont en dehors de la grille
-     * @throws ValeurInitialeModificationException si une valeur initiale de la grille est en position x,y
+     * @param x     position x dans la grille.
+     * @param y     position y dans la grille.
+     * @param value élément de grille à mettre
+     * dans la case, null pour vider la case.
+     * @throws ValeurImpossibleException exception.
+     * @throws ElementInterditException exception.
+     * @throws HorsBornesException exception.
+     * @throws ValeurInitialeModificationException exception.
      */
     void setValue(int x, int y, ElementDeGrille value)
-            throws HorsBornesException, ValeurImpossibleException, ElementInterditException, ValeurInitialeModificationException;
+            throws HorsBornesException,
+            ValeurImpossibleException, ElementInterditException,
+            ValeurInitialeModificationException;
 
     /**
      * Renvoie une valeur de la grille.
      *
      * @param x position x dans la grille
      * @param y position y dans la grille
-     * @return élément de la grille de la case x,y, null s'il n'y a pas d'élément à cette position
-     * @throws HorsBornesException si x ou y sont en dehors de la grille
+     * @return élément de la grille de la case x,y,
+     * null s'il n'y a pas d'élément à cette position.
+     * @throws HorsBornesException si x ou y sont en dehors de la grille.
      */
     ElementDeGrille getValue(int x, int y) throws HorsBornesException;
 
@@ -68,9 +73,10 @@ public interface Grille {
      * @param x     position x dans la grille
      * @param y     position y dans la grille
      * @param value valeur a mettre dans la case
-     * @return true si value peut être placé dans la grille en position x,y en respectant les règles du sudoku et sans modifier une valeur initiale.
+     * @return true si value peut être placé dans la grille en position x,y
+     * en respectant les règles du sudoku et sans modifier une valeur initiale.
      * @throws HorsBornesException      si x ou y sont hors bornes
-     * @throws ElementInterditException si value n'est pas un caractere pouvant être mis dans la grille
+     * @throws ElementInterditException excepetion.
      */
     boolean isPossible(int x, int y, ElementDeGrille value)
             throws HorsBornesException, ElementInterditException;
@@ -80,6 +86,6 @@ public interface Grille {
      * @param y     position y dans la grille
      * @return true si la case x,y contient une valeur initiale de la grille.
      */
-    boolean isValeurInitiale(int x,int y);
+    boolean isValeurInitiale(int x, int y);
 
 }
