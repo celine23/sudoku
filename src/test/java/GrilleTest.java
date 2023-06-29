ddb00bc67635e8fc5a67a88ec9d7214500862dd1
pauleBrenda <pmampassi@gmail.com>
2023-06-29 21:11:38 +0200

    Update GrilleTest.java

--- src/test/java/GrilleTest.java
+++ src/test/java/GrilleTest.java
@@ -1,6 +1,9 @@
 
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import fr.upjv.miage.exception.ElementInterditException;
 import fr.upjv.miage.exception.HorsBornesException;
 import fr.upjv.miage.exception.ValeurImpossibleException;
